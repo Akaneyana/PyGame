527ceea505dbc765890dc3964a40562454d64d83
Akaneyana <uwu.akane8@gmail.com>
2024-10-28 10:58:35 +0100

    added more to the main game

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 import pygame
+import sys
+import time
+import random
 
-
+#start using pygame
 pygame.init()
 
+# how big the screen is
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Reaction Time Test")
 
-run = True
-while run:
+# what font i am using
+font = pygame.font.SysFont('Rubik-Regular.ttf', 90)
+
+title = font.render()
+
+
+# runs pygame into a game loop
+while True:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            run = False
+            pygame.quit()
+            sys.exit()
 
-pygame.quit()
+screen.fill("white")
+
+pygame.display.update()
